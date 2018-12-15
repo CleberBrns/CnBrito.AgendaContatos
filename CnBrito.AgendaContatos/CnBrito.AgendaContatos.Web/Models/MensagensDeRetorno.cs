1779041798c184750a579997e977e9944f1e14c2
Cleber do Nascimento <cleber.nsbrito@gmail.com>
2018-12-15 17:48:05 -0200

    Classe para tratamento de mensagens

---
+++ CnBrito.AgendaContatos/CnBrito.AgendaContatos.Web/Models/MensagensDeRetorno.cs
@@ -0,0 +1,9 @@
+﻿namespace CnBrito.AgendaContatos.Web.Models
+{
+    public class MensagensDeRetorno
+    {
+        public string MensagemExibicao { get; set; }
+        public string MensagemAnalise { get; set; }
+        public bool Status { get; set; }
+    }
+}
